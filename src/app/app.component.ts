f77d86478c7fc4e12806c5bfec2e2f5aac938ff8
saurabh0vipin <saurabhvipin55@gmail.com>
2020-04-01 23:59:38 +0530

    Two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,21 @@ export class AppComponent  {
   age:number=10;
 
   name="Saurabh";
+  val1:number;
+  val2:number;
+sum1:number;
 
+v1:number;
+v2:number;
+sum2:number;
+add()
+{
+  this.sum1=this.val1+this.val2;
+}
+add2()
+{
+  this.sum2=this.v1+this.v2;
+}
   changeText()
   {
     if(this.name!="Saurabh")
