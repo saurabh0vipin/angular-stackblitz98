9dfed84a328090ae754aabdf7f8f5d3474d23fd0
saurabh0vipin <saurabhvipin55@gmail.com>
2020-04-15 16:05:19 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { HelloComponent } from './hello.component';
  
 @NgModule({
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-menu',
+  templateUrl: '.menu/menu.component.html'
+})
+export class MenuComponent  {}
