6d8d6d086b4fb3c70925601a7dab431f01d4cf37
saurabh0vipin <saurabhvipin55@gmail.com>
2020-04-18 16:37:31 +0530

    unit 3 start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,53 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  firstname: string = "Angular";
-  lastname: string="Training";
-  age:number=10;
-
-  name="Saurabh";
-  val1:number;
-  val2:number;
-sum1:number;
-
-v1:number;
-v2:number;
-sum2:number;
-add()
-{
-  this.sum1=this.val1+this.val2;
-}
-add2()
-{
-  this.sum2=this.v1+this.v2;
-}
-  changeText()
-  {
-    if(this.name!="Saurabh")
-    this.name="Saurabh";
-    else
-    this.name="saurabh_vipin"
-  }
-
-
-  fruits:string[]=[];
-  people:any[]=[
-    {"name":"Saurabh","age":22,"country":"IN"},
-    {"name":"Gabrial","age":31,"country":"AU"},
-    {"name":"obama","age":48,"country":"US"},
-    {"name":"Falpes","age":38,"country":"GE"},
-    {"name":"kavin","age":43,"country":"UK"},
-  ];
 
   constructor()
   {
-    this.fruits.push("Apple");
-    this.fruits.push("Orange");
-    this.fruits.push("grapes");
-    this.fruits.push("Guava");
-    this.fruits.push("Banana");
-    this.fruits.push("Melons");
 
-    console.log(this.fruits);
   }
 }
